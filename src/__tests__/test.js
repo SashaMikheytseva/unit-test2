0e083d833b3bf1972718cda48beb75033e4b1acb
Sasha Mikheytseva <berezinas21@gmail.com>
2023-08-22 23:55:53 +0300

    fixed after eslint

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,18 +1,18 @@
 import heroesSort from '../heroesLvl';
 
 test('checking heroes sort', () => {
-    const heroes = [
-        {name: 'мечник', health: 10},
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-    ];
+  const heroes = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
 
-    const expected = [
-        {name: 'маг', health: 100},
-        {name: 'лучник', health: 80},
-        {name: 'мечник', health: 10},
-    ];
+  const expected = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
 
-    const result = heroesSort(heroes);
-    expect(result).toEqual(expected);
-})
+  const result = heroesSort(heroes);
+  expect(result).toEqual(expected);
+});
